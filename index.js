bdd1137e0ac6c0ec1e30b86596507d8760af2a8c
darshit patel <darshitbhanderi2316@gmail.com>
2022-11-22 11:09:33 +0530

    price product add

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const AuthRoute = require("./routes/auth");
 const ProductRoute = require("./routes/products");
 const cartRoute = require("./routes/cart");
 const ordersRoute = require("./routes/orders");
-const priceOfProduct = require("./routes/priceOfProduct");
 var cors = require("cors");
 app.use(cors());
 dotenv.config();
@@ -25,7 +24,6 @@ app.use("/api/user", UserRoute);
 app.use("/api/product", ProductRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/orders", ordersRoute);
-app.use("/api/priceOfProduct", priceOfProduct);
 app.listen(process.env.PORT || 5000, () => {
   console.log("server is establiesd");
 });
