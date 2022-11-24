ce01bbeb4b4f6f872d5372fb264331a5a16c9d55
darshit patel <darshitbhanderi2316@gmail.com>
2022-11-24 17:34:37 +0530

    update add product module

--- models/Product.js
+++ models/Product.js
@@ -4,9 +4,9 @@ const ProductsSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
-    img: { type: String, required: true },
-    type: { type: Array },
-    color: { type: Array },
+    img: { type: Array, required: true },
+    type: { type: Array, required: true },
+    color: { type: Array, required: true },
     price: { type: Number, required: true },
     inStock: { type: Boolean, default: true },
   },
