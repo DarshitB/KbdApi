00959908fa270c96cdcbb059900893d58ad12b98
darshit patel <darshitbhanderi2316@gmail.com>
2022-12-22 10:34:51 +0530

    fabrik module

--- models/Fabrics.js
+++ models/Fabrics.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const FabricsSchema = new mongoose.Schema({
   uniquId: { type: String, required: true, unique: true },
   fabric: { type: String, required: true },
+  fabricName: { type: String, required: true },
   fabImg: { type: String, required: true },
   band: { type: String, required: true },
 });
